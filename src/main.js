cda49db1feb5317ae670709f2aac587d5de0ef0e
LAPTOP-NMR7E0CK\nini <924499172@qq.com>
2019-03-20 14:21:46 +0800

    配置mint-ui（mint-ui 是有饿了么前端团队推出的基于Vue.js的移动端组件库）

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 配置mint-ui
+import MintUI from 'mint-ui'
+// 引入css
+import 'mint-ui/lib/style.css'
+
 // 配置axios
 import Axios from 'axios'
+
+// 安装插件，注册全局属性 及 挂载属性
+Vue.use(MintUI)
+
 // 配置公共URL
 Axios.defaults.baseURL = ''
 Vue.prototype.$axios = Axios
