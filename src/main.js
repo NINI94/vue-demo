a81d2ceacc77071c25693590f3fb2a7d4ea7b0af
LAPTOP-NMR7E0CK\nini <924499172@qq.com>
2019-03-20 13:37:01 +0800

    配置axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 配置axios
+import Axios from 'axios'
+// 配置公共URL
+Axios.defaults.baseURL = ''
+Vue.prototype.$axios = Axios
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
